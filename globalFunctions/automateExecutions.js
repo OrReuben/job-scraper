751dfbf70b06597499294579a55050868c0894d1
orreuven <orreuven1243@gmail.com>
2023-05-04 10:53:44 +0300

    dynamic base_url

--- globalFunctions/automateExecutions.js
+++ globalFunctions/automateExecutions.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const axiosInstance = axios.create({
-  baseURL: "https://job-scraper-api.onrender.com",
+  baseURL: process.env.BASE_URL,
 });
 
 const lastExecutions = {};
